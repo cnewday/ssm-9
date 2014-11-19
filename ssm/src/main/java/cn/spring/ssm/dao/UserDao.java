1c879e40664eb84555b4b3621fe57289666eb128
wasfever <s.xiao2012@gmail.com>
2014-11-19 15:55:03 +0800

    [update] 修改测试案例，实现批量增加框架测试基本完成。

--- ssm/src/main/java/cn/spring/ssm/dao/UserDao.java
+++ ssm/src/main/java/cn/spring/ssm/dao/UserDao.java
@@ -1,7 +1,13 @@
 package cn.spring.ssm.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
 import cn.spring.ssm.entity.User;
 
+@Repository
 public interface UserDao {
 	/**
 	 * 添加新用户
@@ -10,5 +16,29 @@ public interface UserDao {
 	 * @return
 	 */
 	public int insertUser(User user);
+	
+	/**
+	 * 
+	 *  Description:批量插入
+	 *  @author shaox  DateTime 2014年11月19日 下午3:22:04
+	 */
+	public void batchInsertUser(List<User> ls);
+	
+	/**
+	 * 
+	 *  Description:所有user数
+	 *  @author shaox  DateTime 2014年11月19日 上午10:56:54
+	 *  @return
+	 */
+	public int countAll();
+	
+	/**
+	 * 
+	 *  Description:获取所有User列表
+	 *  @author shaox  DateTime 2014年11月19日 上午11:15:05
+	 *  @param offset
+	 *  @param rows
+	 */
+	public List<User> getAllList(@Param("offset") long offset,@Param("rows") long rows); 
 
 }
--- ssm/src/test/java/cn/spring/ssm/test/UserTest.java
+++ ssm/src/test/java/cn/spring/ssm/test/UserTest.java
@@ -1,16 +1,21 @@
 package cn.spring.ssm.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import cn.spring.ssm.dao.UserDao;
 import cn.spring.ssm.entity.User;
 import cn.spring.ssm.service.UserService;
 
 public class UserTest {
 
 	private UserService userService;
+	private UserDao userDao;
 
 	@Before
 	public void before() {
@@ -19,14 +24,37 @@ public class UserTest {
 				new String[] { "classpath:conf/spring.xml",
 						"classpath:conf/spring-mybatis.xml" });
 		userService = (UserService) context.getBean("userServiceImpl");
+		userDao = (UserDao) context.getBean("userDao");
 	}
 
 	@Test
 	public void addUser() {
 		User user = new User();
-		user.setNickname("Jack");
-		user.setState(1);
+		user.setNickname("sb2");
+		user.setState(0);
 		System.out.println(userService.insertUser(user));
 	}
-
+	
+	@Test
+	public void batchInsertUser() {
+		List<User> ls = new ArrayList<User>();
+		for (int i = 0; i < 2; i++) {
+			User user = new User();
+			user.setState(i);
+			user.setNickname("lvlvl"+i);
+			ls.add(user);
+		}
+		userDao.batchInsertUser(ls);
+	}
+	
+	@Test
+	public void getAllList(){
+		List<User> list = userDao.getAllList(1,10);
+		int listsize = list.size();
+		for (int i = 0; i < listsize; i++) {
+			System.out.println("第"+(i+1)+"条记录： "+
+						list.get(i).getState()+list.get(i).getNickname());
+		}
+	}
+	
 }
